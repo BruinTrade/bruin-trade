255c90b5d46f340efd93f0b8d8b6b6a656ea0dba
Kevin <kevinsl192k@gmail.com>
2022-02-09 22:28:11 -0800

    UI for SignUp

---
+++ src/components/signup.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function SignUp(props) {
+    return(
+        <div className='w-350px h-494px bg-white'>
+            <div >
+            Username
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default SignUp;
