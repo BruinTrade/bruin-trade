67d1379d20f095bba6b67db3406fe749bbe038cd
Kevin <kevinsl192k@gmail.com>
2022-02-10 01:21:08 -0800

    UI for sign up wip

--- src/components/signup.js
+++ src/components/signup.js
@@ -2,25 +2,34 @@ import React from "react";
 
 function SignUp(props) {
     return(
-        <div className='w-350px h-494px bg-white'>
-            <div >
-            Username
+        <div className='w-350px h-494px bg-white rounded-25px item-center flex flex-col justify-between px-25px pt-29px font-avenir text-14px text-gray-500 font-medium'>
+            <div>
+                Username
+                <form className='font-regular text-gray-300 rounded-12px bg-gray-100'>
+                    No more than 20 characters
+                </form>
             </div>
             <div>
-
+                Email
+                <form className='font-regular text-gray-300 rounded-12px bg-gray-100'>
+                    No more than 20 characters
+                </form>
             </div>
             <div>
-
+                Password
+                <form className='font-regular text-gray-300 rounded-12px bg-gray-100'>
+                    No more than 20 characters
+                </form>
             </div>
             <div>
-
+                Location
+                <form className='font-regular text-gray-300 rounded-12px bg-gray-100'>
+                    No more than 20 characters
+                </form>
             </div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
+            <button>
+                Sign Up
+            </button>
         </div>
     )
 }
