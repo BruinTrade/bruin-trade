eb5ddd691a09aaace195b3db2d04b45daf68fa33
Kevin <kevinsl192k@gmail.com>
2022-02-10 22:14:13 -0800

    UI for SignIn completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SignUp from "../components/signup.js"
+import SignIn from "../components/signin.js"
 
 function Home() {
     return (
         <div className="flex flex-row justify-center">
             <div className="text-2xl">Home</div>
             <SignUp/>
+            <SignIn/>
         </div>
     );
 }
