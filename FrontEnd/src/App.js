1ba67cbaacaee60c23faabc63c3630907cc999eb
Bob Xia <bobxia@MacBook-Pro-6.local>
2022-02-12 17:06:42 -0800

    complete user registration capabilities

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,19 +4,24 @@ import NavBar from "./components/navbar.js";
 import Home from './pages/Home.js'
 import About from './pages/About.js'
 import Post from './pages/Post.js'
+import PageSignup from './pages/Signup.js'
+
 
 const App = () => (
-  <div className='bg-bg_blue'>
-    <Router>
-      <NavBar />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/post/*" element={<Post/>} />
-          <Route path="/about" element={<About/>} />
-        </Routes>
-      </Suspense>
-    </Router>
+  <div className='flex flex-row justify-center bg-bg_blue h-screen'>
+    <div className='w-1512px'>
+      <Router>
+        <NavBar />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/post/*" element={<Post/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/signup" element={<PageSignup/>} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </div>
   </div>
   
 );
